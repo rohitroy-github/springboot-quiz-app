e2fd84b78c46998f3b22bc97b3550176fd4513a5
Roy <rohit.f.roy@accenture.com>
2024-11-06 20:07:55 +0530

    add: getQuizQuestions function

--- src/main/java/eclipse/springboot/csquizapp/controller/QuizController.java
+++ src/main/java/eclipse/springboot/csquizapp/controller/QuizController.java
@@ -1,13 +1,19 @@
 package eclipse.springboot.csquizapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import eclipse.springboot.csquizapp.model.Question;
+import eclipse.springboot.csquizapp.model.QuestionWrapper;
 import eclipse.springboot.csquizapp.service.QuizService;
 
 @RestController
@@ -21,6 +27,13 @@ public class QuizController {
 	public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) { 
 		
 		return quizService.createQuiz(category, numQ, title);
+	}
+	
+	@GetMapping("get/{id}")
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) { 
+		
+		return quizService.getQuizQuestions(id);
+		
 	}
 
 }
--- src/main/java/eclipse/springboot/csquizapp/service/QuizService.java
+++ src/main/java/eclipse/springboot/csquizapp/service/QuizService.java
@@ -1,6 +1,8 @@
 package eclipse.springboot.csquizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +12,9 @@ import org.springframework.stereotype.Service;
 import eclipse.springboot.csquizapp.dao.QuestionDao;
 import eclipse.springboot.csquizapp.dao.QuizDao;
 import eclipse.springboot.csquizapp.model.Question;
+import eclipse.springboot.csquizapp.model.QuestionWrapper;
 import eclipse.springboot.csquizapp.model.Quiz;
+import jakarta.transaction.Transactional;
 
 @Service
 public class QuizService {
@@ -34,6 +38,41 @@ public class QuizService {
 		
 		return new ResponseEntity<>("Quiz successfully created", HttpStatus.CREATED);
 	}
+	
+	@Transactional
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) { 
+	    try {
+	        Optional<Quiz> quiz = quizDao.findById(id);
+	        
+	        // Check if the quiz is present
+	        if (quiz.isPresent()) {
+	            List<Question> questionsFromDB = quiz.get().getQuestions();
+	            List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+	            for (Question q : questionsFromDB) { 
+	                QuestionWrapper qw = new QuestionWrapper(
+	                    q.getId(), 
+	                    q.getQuestionTitle(), 
+	                    q.getOption1(), 
+	                    q.getOption2(), 
+	                    q.getOption3(), 
+	                    q.getOption4()
+	                );
+	                questionsForUser.add(qw);
+	            }
+	            
+	            return new ResponseEntity<>(questionsForUser, HttpStatus.OK); 
+	        } else {
+	            // Return an error response if the quiz is not found
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+
 	
 	
 }
